1f4d9f21526159380ed8dc977215b43f1e85661c
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-05 13:13:20 +0000

    extension info file for eZ publish 3.9

---
+++ ezinfo.php
@@ -0,0 +1,15 @@
+<?php
+class sckcreatesubtreenoficationruleeventInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "SCK create subtree notification rule",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
