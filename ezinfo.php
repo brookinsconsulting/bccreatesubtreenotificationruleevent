29b843e0975f044668b8067e1dd44da64b6c7a01
Brookins Consulting <info@brookinsconsulting.com>
2008-02-27 05:10:15 +0000

    Updated: Changed ezinfo extension name

--- ezinfo.php
+++ ezinfo.php
@@ -1,11 +1,11 @@
 <?php
-class sckcreatesubtreenoficationruleeventInfo
+class createsubtreenoficationruleeventInfo
 {
     function info()
     {
         return array(
             'Name' => "Create subtree notification rule event type",
-            'Version' => "1.0",
+            'Version' => "1.0.1",
             'Copyright' => "Copyright (C) 2006 SCK-CEN",
             'Author' => "Kristof Coomans",
             'License' => "GNU General Public License v2.0"
